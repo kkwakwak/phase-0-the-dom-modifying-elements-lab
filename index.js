cc8039ad915ec228b9a439ab7de1ab86d22d4d61
Jason Kwak <ralph9696@naver.com>
2022-08-14 12:48:51 -0400

    commit tree 9a0aa49a75f3de73cfaf63bc8503cd2148888406

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+// document.body.append(newHeader);
+newHeader.id = "victory";
+
+newHeader.textContent = "Jason Kwak is the champion";
